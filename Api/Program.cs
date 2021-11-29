1f9edf825c90529f9db9c5f231bf98aa37f2214a
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-11-29 19:38:34 -0300

    Configurando el Logger usando SeriLog

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Api
 {
@@ -13,11 +15,37 @@ namespace Api
     {
         public static void Main(string[] args)
         {
+            //inicializando el logger
+            Log.Logger = new LoggerConfiguration()
+            //ubicacion del log
+            .WriteTo.File(path: "c:\\hotellistings\\logs\\log-.txt",
+                          outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                          //cantidad de veces que se creara el log
+                          rollingInterval: RollingInterval.Day,
+                          restrictedToMinimumLevel: LogEventLevel.Information
+            ).CreateLogger();
+             //al iniciar si es posible dentro del logger agregare la siguiente nota
+            try
+            {
+                Log.Information("Aplication Is Starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //en cambioo si hay algun problema quedara registrado
+                Log.Fatal(ex,"Application Failed to Start");
+            }
+            finally
+            {
+                //por ultimo cerramos el log
+                Log.CloseAndFlush();
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
